7b54e06a6eae8224c976569beedfbd1e2afd23cf
pattharajarin <pattharajarin_p@cmu.ac.th>
2023-03-03 15:23:00 +0700

    ok

--- average.cpp
+++ average.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc, char* argv[]){ 
+    if(argc<=1){
+        cout << "Please input numbers to find average.\n";
+        
+        return 0;
+    }
+    
+    int sum=0;
+    for(int i=1; i<argc; i++){
+        sum += atoi(argv[i]);
+    }
+    cout << "---------------------------------" << endl;
+    int avg = sum / (argc-1);
+    cout << "Average of " << argc-1 << " numbers = " << avg << endl;
+    cout << "---------------------------------" << endl;
+    
+    return 0;
+}
